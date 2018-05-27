09cc1d003fe7d6979b907b0c58169e4e99b5bd1c
Kouhei Suzuki <kouhei.szk@gmail.com>
2018-05-27 19:45:46 +0900

    玉転がし

---
+++ unity-roll-ball-tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+	private void Start()
+	{
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+	}
+
+	private void FixedUpdate()
+	{
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.CompareTag("Pick Up")) 
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+        }
+	}
+
+    private void setCountText() 
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12) {
+            winText.text = "You Win!";
+        }
+    }
+}
